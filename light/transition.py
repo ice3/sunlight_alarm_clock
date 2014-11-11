2b3a1c699b18b2a470f3cdd8abda7748c85ce9c6
mfalce <falce.matthieu@gmail.com>
2014-11-11 18:32:36 +0100

    indentation fix + fix the transition between colors (takes only the smaller path between the colors)

--- light/transition.py
+++ light/transition.py
@@ -8,27 +8,48 @@ import serial
 ser = serial.Serial("/dev/ttyACM0", timeout=1)
 
 def val_interm(hsl1, hsl2, pos):
-	""" Valeur intermédiaire entre la couleur 1 et la couleur 2
-	pos correspond à la postion dans la transition, entre 0 (couleur 1) et 1 (couleur2)
-	"""
-	res_h = (hsl2[0]-hsl1[0])*pos + hsl1[0]
+    """ Valeur intermédiaire entre la couleur 1 et la couleur 2
+    pos correspond à la postion dans la transition, entre 0 (couleur 1) et 1 (couleur2)
+    """
+    # res_h = (hsl2[0]-hsl1[0])*pos + hsl1[0]
+    # hue value transition can be tricky to compute
+    # we have to decide if we go clock wise or counter clowk wise.
+    # inspired from here : http://stackoverflow.com/questions/2593832
+    if hsl1[0] >= hsl2[0]:
+        distCCW = hsl1[0] - hsl2[0] 
+    else : 
+       distCCW = 1 + hsl1[0] - hsl2[0]
 
-	res_l = abs(hsl2[2]-hsl1[2])*pos + hsl1[2]
-	res_s = abs(hsl2[1]-hsl1[1])*pos + hsl1[1]
-	r, g, b = colorsys.hls_to_rgb(res_h, res_l, res_s)
+    if hsl1[0] >= hsl2[0]:
+        distCW = 1 + hsl2[0] - hsl1[0] 
+    else:
+        distCW = hsl2[0] - hsl1[0]
 
-	print "pos : ", pos, "   orig : ", hsl1, "  | dest : ", hsl2
-	print "hls : ", "{}, {}, {}".format(res_h,  res_l, res_s)
-	print "rgb : ", "{}, {}, {}".format(r, g, b)
+    #ensure we get the right number of steps by adding remainder to final part
+    if distCW <= distCCW:
+        res_h = hsl1[0] + (distCW * pos) 
+    else:
+        res_h = hsl1[0] - (distCCW * pos)
+    if res_h < 0: res_h = 1 + res_h
+    if res_h > 1: res_h = res_h - 1
 
-	return r, g, b
+
+    res_l = abs(hsl2[2]-hsl1[2])*pos + hsl1[2]
+    res_s = abs(hsl2[1]-hsl1[1])*pos + hsl1[1]
+    r, g, b = colorsys.hls_to_rgb(res_h, res_l, res_s)
+
+    print "pos : ", pos, "   orig : ", hsl1, "  | dest : ", hsl2
+    print "hls : ", "{}, {}, {}".format(res_h,  res_l, res_s)
+    print "rgb : ", "{}, {}, {}".format(r, g, b)
+
+    return r, g, b
 
 def correction(col):
-	print "rgb avant : ", col
-	cor = [255, 212.0, 221.0]
-	res = [int(val * c) for val, c in zip(col, cor)]
-	print "rgb : ", res
-	return res
+    print "rgb avant : ", col
+    cor = [255, 212.0, 212.0]
+    res = [int(val * c) for val, c in zip(col, cor)]
+    print "rgb : ", res
+    return res
 
 color1_hsl = (240.0/360.0, 100.0/100.0,  0.0/100.0) # black blue
 color2_hsl = (240.0/360.0, 100.0/100.0, 40.0/100.0) # blue
@@ -43,34 +64,38 @@ transition4 = {"orig": color4_hsl, "end": color5_hsl, "dur": 100}
 
 sleep_time = 1
 
-# for t in range(1, transition1["dur"]+1):
-# 	res = val_interm(transition1["orig"], transition1["end"], t*1.0/transition1["dur"])
-# 	res = correction(res)
-# 	ser.write(",".join(map(str, res))+"\n")
-# 	ser.readline().strip()
-# 	ser.readline()
-# 	time.sleep(1)
+def main():
+    for t in range(1, transition1["dur"]+1):
+      res = val_interm(transition1["orig"], transition1["end"], t*1.0/transition1["dur"])
+      res = correction(res)
+      ser.write(",".join(map(str, res))+"\n")
+      ser.readline().strip()
+      ser.readline()
+      time.sleep(sleep_time * 2)
 
-# for t in range(1, transition2["dur"]+1):
-# 	res = val_interm(transition2["orig"], transition2["end"], t*1.0/transition1["dur"])
-# 	res = correction(res)
-# 	ser.write(",".join(map(str, res))+"\n")
-# 	ser.readline().strip()
-# 	ser.readline()
-# 	time.sleep(1)
+    for t in range(1, transition2["dur"]+1):
+      res = val_interm(transition2["orig"], transition2["end"], t*1.0/transition1["dur"])
+      res = correction(res)
+      ser.write(",".join(map(str, res))+"\n")
+      ser.readline().strip()
+      ser.readline()
+      time.sleep(sleep_time)
 
-for t in range(1, transition3["dur"]+1):
-	res = val_interm(transition3["orig"], transition3["end"], t*1.0/transition1["dur"])
-	res = correction(res)
-	ser.write(",".join(map(str, res))+"\n")
-	ser.readline().strip()
-	ser.readline()
-	time.sleep(1)
+    for t in range(1, transition3["dur"]+1):
+        res = val_interm(transition3["orig"], transition3["end"], t*1.0/transition1["dur"])
+        res = correction(res)
+        ser.write(",".join(map(str, res))+"\n")
+        ser.readline().strip()
+        ser.readline()
+        time.sleep(sleep_time)
 
-for t in range(1, transition4["dur"]+1):
-	res = val_interm(transition4["orig"], transition4["end"], t*1.0/transition1["dur"])
-	res = correction(res)
-	ser.write(",".join(map(str, res))+"\n")
-	ser.readline().strip()
-	ser.readline()
-	time.sleep(1)
+    for t in range(1, transition4["dur"]+1):
+        res = val_interm(transition4["orig"], transition4["end"], t*1.0/transition1["dur"])
+        res = correction(res)
+        ser.write(",".join(map(str, res))+"\n")
+        ser.readline().strip()
+        ser.readline()
+        time.sleep(sleep_time/2.0)
+
+if __name__ == "__main__":
+    main()
