1820f9d6e80a622a51c3870b4687555406581e7e
mfalce <falce.matthieu@gmail.com>
2015-02-01 16:32:42 +0100

    manage crontabs in the app

--- web_control/app.py
+++ web_control/app.py
@@ -5,6 +5,27 @@ from flask import Flask, render_template, session, request
 from flask.ext.socketio import SocketIO, emit, join_room, leave_room, \
     close_room, disconnect
 
+from crontab import CronTab
+from datetime import datetime
+
+# cron  = CronTab(user=True)
+
+# # get already created crontab
+# for job in cron.find_comment("clock"):
+#     if job.hour == hour and job.minute == minute:
+#         pass
+#     else :
+#         #remove old cron
+#         cron.remove_all(comment="clock")
+
+# job = cron.new(command="python /home/pi/sunlight_alarm_clock/light/python/transition.py", comment='clock weekday')
+# job.hour.on(hour)
+# job.minute.on(minute)
+
+# schedule = job.schedule(date_from=datetime.now())
+# cron.write()
+
+
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
@@ -25,7 +46,25 @@ def test_disconnect():
 
 @socketio.on('onChangeTime', namespace='/test')
 def time_change(message):
-    print("New wake up time:", message)
+    hour, minute = message["data"].split(":")
+    print("New wake up time: {}-{}".format(hour, minute))
+    cron  = CronTab(tabfile="test.CronTab")
+
+    # get already created crontab
+    for job in cron.find_comment("clock weekday"):
+        print(job)
+        if job.hour == hour and job.minute == minute:
+            pass
+        else :
+            #remove old cron
+            cron.remove_all(comment="clock weekday")
+            job = cron.new(command="python /home/pi/sunlight_alarm_clock/light/python/transition.py", comment='clock weekday')
+            job.hour.on(hour)
+            job.minute.on(minute)
+            schedule = job.schedule(date_from=datetime.now())
+            import IPython; IPython.embed()
+            cron.write("test.CronTab")
+
 
 @socketio.on('disconnect request', namespace='/test')
 def disconnect_request():
